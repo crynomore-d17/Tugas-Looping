826748c6bf68c2743b8d967167246739830c2749
crynomore-d17 <58874755+crynomore-d17@users.noreply.github.com>
2020-12-24 23:07:07 +0700

    Add files via upload

---
+++ src/empat.java
@@ -0,0 +1,11 @@
+public class empat {
+    public static void main(String[] args) {
+
+        for (int x=1; x<=5; x++){
+            for (int b=x; b<=5; b++) {
+                System.out.print(b);
+            }
+            System.out.println();
+        }
+    }
+}
